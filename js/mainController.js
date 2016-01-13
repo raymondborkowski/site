ed8e71a3d2c7a5cafc0e7cab31890c5f359ec53f
raymondborkowski <rborkows@umich.edu>
2016-01-12 22:28:10 -0500

    Lets make this angular friendly

---
+++ js/mainController.js
@@ -0,0 +1,63 @@
+angular.module('myApp.controllers').controller('skills', ['$scope', function($scope) { 
+	$scope.languages = [ 
+  { 
+    language: "JAVASCRIPT",
+    level: "8"
+  }, 
+  { 
+    language: "JQUERY",
+    level: "6"
+  } ,
+   { 
+    language: "ANGULAR",
+    level: "6"
+  } ,
+  { 
+    language: "CSS",
+    level: "8"
+  },
+  { 
+    language: "HTML",
+    level: "7"
+  }, 
+  { 
+    language: "PHP",
+    level: "7"
+  } ,
+   { 
+    language: "NODE.JS",
+    level: "5"
+  } ,
+  { 
+    language: "C++",
+    level: "7.5"
+  },
+  { 
+    language: "PYTHON",
+    level: "4"
+  } ,
+  { 
+    language: "oracle",
+    level: "9"
+  },
+  { 
+    language: "Mysql",
+    level: "9"
+  }
+
+]
+  $scope.learn = [
+    {
+      language: "java"
+    },
+    {
+      language: "backbone"
+    },
+    {
+      language: "swift"
+    },
+    {
+      language: "and ruby"
+    }
+  ]
+}]);
